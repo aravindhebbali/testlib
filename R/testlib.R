2352925d8c294b34b923ca10a559d6f969337fdb
rsquaredin <rsquaredin@gmail.com>
2016-11-14 20:00:47 +0530

    Partial documentation for cross_table

---
+++ R/testlib.R
@@ -0,0 +1,11 @@
+#' testlib: A package for cross tabulation of data
+#'
+#' The testlib package provides functions for cross tabulation of categorical data
+#' and visulize the cross tables using bar and mosaic plots
+#'
+#' @section testlib functions:
+#' cross_table
+#'
+#' @docType package
+#' @name testlib
+NULL
